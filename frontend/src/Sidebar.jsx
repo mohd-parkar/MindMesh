c4ef68a4cfa9c122f1100a1a7dde43be145351ac
Mohammed Parkar <parkarmohammed74@gmail.com>
2026-07-08 03:09:21 +0530

    added sidebar ui html and css

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -1,8 +1,23 @@
+import "./sidebar.css";
+
 function Sidebar(){
     return (
-        <div>
-            Sidebar
-        </div>
+        <section className="sidebar">
+            <button>
+                <img src="src/assets/ai-brain.png" alt="logo" className="logo"></img>
+                <i class="fa-solid fa-pen-to-square"></i>
+            </button>
+
+            <ul className="history">
+                <li>Thread 1</li>
+                <li>Thread 2</li>
+                <li>Thread 3</li>
+            </ul>
+
+            <div className="sign">
+                <p> By Mohd Parkar</p>
+            </div>
+        </section>
     )
 }
 
