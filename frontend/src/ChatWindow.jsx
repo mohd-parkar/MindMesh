b33e219066635a1538643c783ef5ac6dca5cfdff
Mohammed Parkar <parkarmohammed74@gmail.com>
2026-07-08 18:53:58 +0530

    Sahil name added

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -1,8 +1,31 @@
+import "./ChatWindow.css";
+
+import Chat from "./Chat";
 
 function ChatWindow(){
     return (
-        <div>
-            ChatWindow
+        <div className="ChatWindow">
+            <div className="navbar">
+                <span>MindMesh&nbsp;<i class="fa-solid fa-chevron-down"></i></span>
+                <div className="userIconDiv">
+                    <span className="userIcon"><i class="fa-solid fa-user"></i></span>
+                </div>
+            </div>
+
+            <Chat/>
+
+            <div className="chatInput">
+                <div className="inputBox">
+                    <input type="text" placeholder="Ask anything"/>
+                    <div id = "submit">
+                        <i class="fa-solid fa-paper-plane"></i>
+                    </div>
+                </div>
+                <p className="info">
+                    * MindMesh can make mistakes. Check important info. 
+                </p>
+            </div>
+
         </div>
     )
 }
