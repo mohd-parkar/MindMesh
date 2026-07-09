39505c29c00ec50566f378cddffcadc9596582d7
Mohammed Parkar <parkarmohammed74@gmail.com>
2026-07-09 16:55:31 +0530

    added formating in the gpt generated response displayed on chat

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -1,6 +1,12 @@
 import { MyContext } from "./MyContext.jsx";
 import { useContext } from "react";
 import "./Chat.css";
+import ReactMarkdown from "react-markdown";
+import rehypeHighLight from "rehype-highlight";
+
+// importing css file to style the code snippet format in chat
+import "highlight.js/styles/github-dark.css";
+import Markdown from "react-markdown";
 
 function Chat() {
   const { newChat, prevChat } = useContext(MyContext);
@@ -14,9 +20,13 @@ function Chat() {
             className={chat.role === "user" ? "userDiv" : "gptDiv"}
             key={idx}
           >
-            <p className={chat.role === "user" ? "userMessage" : "gptMessage"}>
-              {chat.content}
-            </p>
+            <div
+              className={chat.role === "user" ? "userMessage" : "gptMessage"}
+            >
+              <ReactMarkdown rehypePlugins={[rehypeHighLight]}>
+                {chat.content}
+              </ReactMarkdown>
+            </div>
           </div>
         ))}
       </div>
