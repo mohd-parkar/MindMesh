8ba65250eeeca6fa9312d06f628da0550cf25292
Vishwadip Damodare <vishdamodare10@gmail.com>
2026-07-19 02:32:30 +0530

    added recent thread in sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,8 +12,9 @@ function App() {
   const[currThreadId,setCurrThreadId] = useState(uuidv1());
   const[prevChat,setPrevChat] = useState([]); // array to store the chat (user prompt + reply)
   const[newChat,setNewChat] = useState(true); // track if newchat
+  const [allThreads, setAllThreads] = useState([]);
 
-  const providerValues = {prompt,setPrompt,reply,setReply,currThreadId,setCurrThreadId,prevChat,setPrevChat,newChat,setNewChat};// passing values
+  const providerValues = {prompt,setPrompt,reply,setReply,currThreadId,setCurrThreadId,prevChat,setPrevChat,newChat,setNewChat,allThreads,setAllThreads};// passing values
 
   return (
     <div className='app'>
--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -1,24 +1,126 @@
-import "./sidebar.css";
+import "./Sidebar.css";
+import { useContext, useEffect } from "react";
+import { MyContext } from "./MyContext";
+import { v1 as uuidv1 } from "uuid";
 
-function Sidebar(){
+function Sidebar() {
+    const {
+        allThreads,
+        setAllThreads,
+        currThreadId,
+        setNewChat,
+        setPrompt,
+        setReply,
+        setCurrThreadId,
+        setPrevChat,
+    } = useContext(MyContext);
+
+    const getAllThreads = async () => {
+        try {
+            const response = await fetch("http://localhost:8080/api/v1/thread");
+            const res = await response.json();
+
+            const filteredData = res.map((thread) => ({
+                threadId: thread.threadId,
+                title: thread.title,
+            }));
+
+            setAllThreads(filteredData);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        getAllThreads();
+    }, [currThreadId]);
+
+    const createNewChat = () => {
+        setNewChat(true);
+        setPrompt("");
+        setReply(null);
+        setCurrThreadId(uuidv1());
+        setPrevChat([]);
+    };
+
+    const changeThread = async (newThreadId) => {
+        try {
+            setCurrThreadId(newThreadId);
+
+            const response = await fetch(
+                `http://localhost:8080/api/v1/thread/${newThreadId}`
+            );
+
+            const res = await response.json();
+
+            setPrevChat(res);
+            setNewChat(false);
+            setReply(null);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const deleteThread = async (threadId) => {
+        try {
+            const response = await fetch(
+                `http://localhost:8080/api/v1/thread/${threadId}`,
+                {
+                    method: "DELETE",
+                }
+            );
+
+            await response.json();
+
+            setAllThreads((prev) =>
+                prev.filter((thread) => thread.threadId !== threadId)
+            );
+
+            if (threadId === currThreadId) {
+                createNewChat();
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
     return (
         <section className="sidebar">
-            <button>
-                <img src="src/assets/ai-brain.png" alt="logo" className="logo"></img>
-                <i className="fa-solid fa-pen-to-square"></i>
+            <button onClick={createNewChat}>
+                <img
+                    src="src/assets/blacklogo.png"
+                    alt="gpt logo"
+                    className="logo"
+                />
+                <span>
+                    <i className="fa-solid fa-pen-to-square"></i>
+                </span>
             </button>
 
+           
+
             <ul className="history">
-                <li>Thread 1</li>
-                <li>Thread 2</li>
-                <li>Thread 3</li>
+                 <h4>Recents</h4>
+                {allThreads?.map((thread) => (
+                    <li
+                        key={thread.threadId}
+                        onClick={() => changeThread(thread.threadId)}
+                        className={
+                            thread.threadId === currThreadId
+                                ? "highlighted"
+                                : ""
+                        }
+                    >
+                        <span>{thread.title}</span>
+                    </li>
+                ))}
             </ul>
 
             <div className="sign">
-                <p> By Mohd Parkar</p>
+                <p>By Mohd Parkar</p>
             </div>
         </section>
-    )
+    );
 }
 
 export default Sidebar;
